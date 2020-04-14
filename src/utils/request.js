7346f299c4755d480bc1ffd27bccd9d5c74cd509
zhangzhiming <173559542@qq.com>
2020-04-14 11:35:58 +0800

    axios拦截器

--- src/utils/request.js
+++ src/utils/request.js
@@ -16,10 +16,8 @@ service.interceptors.request.use(
     // do something before request is sent
 
     if (store.getters.token) {
-      // let each request carry token
-      // ['X-Token'] is a custom headers key
-      // please modify it according to the actual situation
-      config.headers['X-Token'] = getToken()
+      // 设置token
+      config.headers.Authorization = 'Token ' + getToken()
     }
     return config
   },
@@ -35,7 +33,7 @@ service.interceptors.response.use(
   /**
    * If you want to get http information such as headers or status
    * Please return  response => response
-  */
+   */
 
   /**
    * Determine the request status by custom code
@@ -43,43 +41,49 @@ service.interceptors.response.use(
    * You can also judge the status by HTTP Status Code
    */
   response => {
-    const res = response.data
+    switch (response.status) {
+      case 400:
+        Message({
+          message: response.data.error,
+          type: 'error',
+          duration: 3 * 1000
+        })
+        break
 
-    // if the custom code is not 20000, it is judged as an error.
-    if (res.code !== 20000) {
-      Message({
-        message: res.message || 'Error',
-        type: 'error',
-        duration: 5 * 1000
-      })
-
-      // 50008: Illegal token; 50012: Other clients logged in; 50014: Token expired;
-      if (res.code === 50008 || res.code === 50012 || res.code === 50014) {
-        // to re-login
-        MessageBox.confirm('You have been logged out, you can cancel to stay on this page, or log in again', 'Confirm logout', {
-          confirmButtonText: 'Re-Login',
-          cancelButtonText: 'Cancel',
-          type: 'warning'
+      case 401:
+        Message({
+          message: '用户名或密码错误',
+          type: 'error',
+          duration: 3 * 1000
         }).then(() => {
-          store.dispatch('user/resetToken').then(() => {
+          store.dispatch('user/login').then(() => {
             location.reload()
           })
         })
-      }
-      return Promise.reject(new Error(res.message || 'Error'))
-    } else {
-      return res
+        break
+
+      case 403:
+        Message({
+          message: '您没有权限访问',
+          type: 'warning',
+          duration: 3 * 1000
+        })
+        break
+
+      case 500:
+        Message({
+          message: '服务器失联了',
+          type: 'error',
+          duration: 3 * 1000
+        })
+        break
     }
+
+    return response
   },
+
   error => {
-    console.log('err' + error) // for debug
-    Message({
-      message: error.message,
-      type: 'error',
-      duration: 5 * 1000
-    })
-    return Promise.reject(error)
-  }
-)
+    return Promise.reject(error.response.data.error)
+  })
 
 export default service
